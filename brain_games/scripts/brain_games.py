1840fe873c7dc767b7882f129756ea38e7584cde
rushan <rushan@example.com>
2022-05-25 21:40:03 +0300

    add step3

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import brain_games.cli
+
+
+def greet():
+    print('Welcome to the Brain Games!')
+
+def main():
+    greet()
+    brain_games.cli.welcome_user()
+
+if __name__ == '__main__':
+    main()
