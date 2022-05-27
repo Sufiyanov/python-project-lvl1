4515f113102e3def0bd335e37e6fb411487e5ef9
rushan <rushan@example.com>
2022-05-27 23:14:36 +0300

    add game gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -6,8 +6,8 @@ question = 'What is the result of the expression?'
 def make_answer():
     number_1 = random.randint(1, 100)
     number_2 = random.randint(1, 100)
-    for i in range(number_1):
+    for i in range(1, number_1 + 1):
         if number_2 % i == 0 and number_1 % i == 0:
-            answer = i
+            answer = str(i)
     number = '{0} {1}'.format(number_1, number_2)
     return number, answer
